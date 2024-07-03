79eef2b7b97672ef801cfa9fe8cba257f8954661
tayboy217 <taichi217niina@gmail.com>
2024-07-03 18:20:10 +0900

    first commit

---
+++ src/main/java/com/unitcostcalculation/costcalculator/UnitcostcalculationApplication.java
@@ -0,0 +1,13 @@
+package com.unitcostcalculation.costcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitcostcalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitcostcalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unitcostcalculation/costcalculator/UnitcostcalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitcostcalculation.costcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitcostcalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
