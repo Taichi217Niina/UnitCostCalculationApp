4fb32a1ef799446d41bac6792474ad424f1ff07e
tayboy217 <taichi217niina@gmail.com>
2024-07-06 01:15:07 +0900

    Add DTO・FROMクラス追加

---
+++ src/main/java/com/unitcostcalculation/costcalculator/dto/UnitCostDto.java
@@ -0,0 +1,19 @@
+package com.unitcostcalculation.costcalculator.dto;
+
+import lombok.Data;
+
+/**
+ * 単価計算結果格納用DTOクラス.
+ * 
+ * @author niinataichi
+ *
+ */
+@Data
+public class UnitCostDto {
+	
+	private int totalCost;
+	
+	private int anualIncome;
+	
+
+}
