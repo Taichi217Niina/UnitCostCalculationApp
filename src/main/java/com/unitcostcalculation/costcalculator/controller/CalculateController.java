985f3aaf69dcc94cf234fe8c33acfa6f78ec8e70
tayboy217 <taichi217niina@gmail.com>
2024-07-06 03:31:47 +0900

    Add 年収平均メッセージ用の条件分岐追加

--- src/main/java/com/unitcostcalculation/costcalculator/controller/CalculateController.java
+++ src/main/java/com/unitcostcalculation/costcalculator/controller/CalculateController.java
@@ -33,16 +33,28 @@ public class CalculateController {
 
 	@PostMapping("/result")
 	public String unitCostInputInfo(Model model, UnitCostForm form) {
+		// 入力情報を元に単価計算算出
 		int totalCost;
 		double percentage = form.getPercentage();
 		totalCost = (int) (form.getUnitCost() * (percentage / CalculateConstant.PERCENT));
 		int anualIncome;
 		anualIncome = totalCost * CalculateConstant.TWELVE_MONTHS;
 
+		// 計算結果をDTOに格納
 		UnitCostDto unitCost = new UnitCostDto();
 		unitCost.setTotalCost(totalCost);
 		unitCost.setAnualIncome(anualIncome);
 
+		// 年収平均メッセージ
+		if (anualIncome <= 3500000 && anualIncome >= 0) {
+			model.addAttribute("message", "平均年収より低い年収です。");
+		} else if (anualIncome <= 3900000 && anualIncome > 3500000) {
+			model.addAttribute("message", "平均年収よりやや低い年収です。");
+		} else if (anualIncome <= 4500000 && anualIncome > 3900000) {
+			model.addAttribute("message", "平均年収よりやや高い年収です。");
+		} else if (anualIncome > 4500000) {
+			model.addAttribute("message", "平均年収より高い年収です。");
+		}
 		model.addAttribute("unitCost", unitCost);
 		return "calculated";
 	}
