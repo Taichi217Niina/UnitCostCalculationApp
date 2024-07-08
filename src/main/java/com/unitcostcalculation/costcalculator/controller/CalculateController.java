7a93c9f9dfe4f44f30b7af251256a160cb31ead7
tayboy217 <taichi217niina@gmail.com>
2024-07-08 15:36:45 +0900

    Add Controller層修正、Service層追記

--- src/main/java/com/unitcostcalculation/costcalculator/controller/CalculateController.java
+++ src/main/java/com/unitcostcalculation/costcalculator/controller/CalculateController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.unitcostcalculation.costcalculator.common.CalculateConstant;
 import com.unitcostcalculation.costcalculator.dto.UnitCostDto;
 import com.unitcostcalculation.costcalculator.form.UnitCostForm;
+import com.unitcostcalculation.costcalculator.service.CalculateService;
 
 /**
  * 単価計算用Controllerクラス.
@@ -20,9 +21,14 @@ import com.unitcostcalculation.costcalculator.form.UnitCostForm;
 @RequestMapping("/caluculater")
 public class CalculateController {
 	
+	@GetMapping("/top")
+	public String index() {
+		return "index";
+	}
+
 	@GetMapping
 	public String unitCostInputList(Model model) {
-		model.addAttribute("unitCost", new UnitCostForm());	
+		model.addAttribute("unitCostForm", new UnitCostForm());	
 		return "calculate";
 	}
 	
@@ -34,30 +40,29 @@ public class CalculateController {
 	@PostMapping("/result")
 	public String unitCostInputInfo(Model model, UnitCostForm form) {
 		// 入力情報を元に単価計算算出
-		int totalCost;
-		double percentage = form.getPercentage();
-		totalCost = (int) (form.getUnitCost() * (percentage / CalculateConstant.PERCENT));
-		int anualIncome;
-		anualIncome = totalCost * CalculateConstant.TWELVE_MONTHS;
-
-		// 計算結果をDTOに格納
-		UnitCostDto unitCost = new UnitCostDto();
-		unitCost.setTotalCost(totalCost);
-		unitCost.setAnualIncome(anualIncome);
+		UnitCostDto unitCostResult = CalculateService.doCalculate(form);
 
 		// 年収平均メッセージ
-		if (anualIncome <= 3500000 && anualIncome >= 0) {
-			model.addAttribute("message", "平均年収より低い年収です。");
-		} else if (anualIncome <= 3900000 && anualIncome > 3500000) {
-			model.addAttribute("message", "平均年収よりやや低い年収です。");
-		} else if (anualIncome <= 4500000 && anualIncome > 3900000) {
-			model.addAttribute("message", "平均年収よりやや高い年収です。");
-		} else if (anualIncome > 4500000) {
-			model.addAttribute("message", "平均年収より高い年収です。");
-		}
-		model.addAttribute("unitCost", unitCost);
+		String message = getMessageForAnnualIncome(unitCostResult.getAnualIncome());
+		model.addAttribute("message", message);
+		model.addAttribute("unitCost", unitCostResult);
 		return "calculated";
 	}
 	
+	// 年収に対するメッセージを取得するヘルパーメソッド
+    private String getMessageForAnnualIncome(int annualIncome) {
+		if (annualIncome <= 3300000 && annualIncome >= 0) {
+			return "平均年収より低い年収です。";
+		} else if (annualIncome <= 3800000 && annualIncome > 3300000) {
+			return "平均年収よりやや低い年収です。";
+		} else if (annualIncome <= 4500000 && annualIncome > 3800000) {
+			return "平均年収です。";
+		} else if (annualIncome <= 5200000 && annualIncome > 4500000) {
+			return "平均年収よりやや高い年収です。";
+		} else {
+			return "平均年収より高い年収です。";
+		}
+    	
+    }
 
 }
